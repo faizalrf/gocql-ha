80fed590f7737fece1cb95cf6bd73282cf56007d
Faisal Saeed <faisal.saeed@scylladb.com>
2024-06-19 22:45:15 +0800

    Final commit after commens and implementing pointers

--- mian.go
+++ mian.go
@@ -2,52 +2,89 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"time"
 
 	"github.com/gocql/gocql"
 )
 
+/*
+* 	Test code written to test the gocql driver behaviour if scylla nodes go down.
+* 	The code fails to execute and terminates. Retrying the transactions does not work, have to re initialise the connection/session
+* 	Once the session has been re-initialised, the code continues to work from where it left off.
+* 	Faisal 19-Jun-2024
+ */
+
 func main() {
-	fmt.Println("Test Suite for gocql!")
+	fmt.Println("Test code for gocql!")
 	// Define the ScyllaDB cluster
 	cluster := gocql.NewCluster("127.0.0.1:9002", "127.0.0.1:9003", "127.0.0.1:9004") // replace with your ScyllaDB IP address
 	cluster.Keyspace = "testksp"
 	cluster.Consistency = gocql.LocalOne
 
-	// Create a session
+	// Create a session to ScyllaDB
 	session, err := cluster.CreateSession()
 	if err != nil {
-		fmt.Println("Error creating session: ", err)
+		fmt.Println("Error Connecting to the ScyllaDB: ", err)
 		return
 	}
 	defer session.Close()
 
 	fmt.Println("Connected to ScyllaDB!")
 	rowCount := 0
+	numRetry := 5
+	numSecondWait := 10 * time.Second
+
 	for {
-		// Insert data with retry mechanism
-		if err := insertDataWithRetry(session, 10, 10*time.Second); err != nil {
-			fmt.Println("Failed to insert data after retries: ", err)
+		// Insert data with retry mechanism by sending the session as a pointer
+		if err := writeDataWithRetry(cluster, &session, numRetry, numSecondWait); err != nil {
+			fmt.Printf("Failed to insert data after %d retries\n", err)
 			return
 		} else {
 			rowCount++
-			fmt.Printf("Rows inserted: %d\n", rowCount)
+			fmt.Printf("Inserted Row Count: %d\n", rowCount)
 		}
 		time.Sleep(10 * time.Millisecond)
 	}
 }
 
-func insertDataWithRetry(session *gocql.Session, maxRetries int, delay time.Duration) error {
-	account_id := gocql.MustRandomUUID()
+// Write data to the ScyllaDB table with retry mechanism using a session pointer
+func writeDataWithRetry(cluster *gocql.ClusterConfig, session **gocql.Session, maxRetries int, delay time.Duration) error {
+	accountID := gocql.MustRandomUUID()
 	for i := 0; i < maxRetries; i++ {
-		err := session.Query(`UPDATE daily_account_stat SET "list" = "list" + ? where account_id = ? and date = ?`, 1, account_id.String(), getCurrentDate()).Exec()
+		err := (*session).Query(`UPDATE daily_account_stat SET "list" = "list" + ? WHERE account_id = ? AND date = ?`, 1, accountID.String(), getCurrentDate()).Exec()
 		if err == nil {
 			return nil // Success
 		}
 		fmt.Printf("Error inserting data, attempt %d: %v\n", i+1, err)
-		time.Sleep(delay) // Wait before retrying
+
+		// Check if the error is related to connection then attempt to reconnect and retry
+		if isConnectionError(err) {
+			log.Println("Attempting to reconnect to the ScyllaDB...")
+			time.Sleep(delay) // Wait before retrying
+			if err := reConnect(cluster, session); err != nil {
+				fmt.Println("Error re-connecting to the ScyllaDB: ", err)
+				continue
+			}
+		}
 	}
 	return fmt.Errorf("failed to insert data after %d attempts", maxRetries)
+}
+
+// Reconnect using the session pointer
+func reConnect(cluster *gocql.ClusterConfig, session **gocql.Session) error {
+	(*session).Close()
+	var err error
+	*session, err = cluster.CreateSession()
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+// Check if the error is related to any of the following which need a reconnection to the DB
+func isConnectionError(err error) bool {
+	return err.Error() == "session has been closed" || err.Error() == "EOF" || err.Error() == "gocql: no hosts available in the pool"
 }
 
 func getCurrentDate() time.Time {
